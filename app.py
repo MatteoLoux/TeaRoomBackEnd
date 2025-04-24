5b16ff330cc61b6c2e831be94da6925503d6698a
Loux Matteo <mat.loux@hotmail.com>
2025-04-24 15:54:56 +0200

    port render

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-
+import os
 from routes.users import users
 from routes.cart import cart
 from routes.session import session
@@ -22,4 +22,5 @@ def index():
     return {"message": "API Flask est en ligne 🚀"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
