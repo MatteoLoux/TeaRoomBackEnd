a4226ba086c29a493ea45dd842ace91966c25700
Loux Matteo <mat.loux@hotmail.com>
2025-04-24 19:10:25 +0200

    fix bug

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from routes.goodies import goodies
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True, origins="*")
 
 # Configuration des sessions avec PostgreSQL
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
