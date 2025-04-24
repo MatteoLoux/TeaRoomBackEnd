f1edcb2e3df14f4b1da03f2aa7a09d6ea40256c3
Loux Matteo <mat.loux@hotmail.com>
2025-04-24 18:51:33 +0200

    fix bug

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import psycopg2
 import os
-
+from flask_sqlalchemy  import SQLAlchemy
 
 
 def get_conn():
     return psycopg2.connect(os.getenv("DATABASE_URL"))
 
-
+db_session = SQLAlchemy()
 
